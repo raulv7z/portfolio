1f900b44525ee1dd22f17a33fe09014d63c07064
raulv7z <raulzks0@gmail.com>
2024-06-27 19:25:09 +0200

    Remove unused imports

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -4,8 +4,6 @@ import myImg from "../../Assets/avatar-no-bg.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
